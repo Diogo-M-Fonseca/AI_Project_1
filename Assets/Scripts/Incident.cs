694ce53f332aa75ae3fc3f7e54bdfb2abcf427d5
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2026-04-15 21:14:52 +0100

    incident class copied and reworked from last try

---
+++ Assets/Scripts/Incident.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Incident 
+{
+    private IncidentType type;
+    private Module origin;
+    private float timer;
+
+    public IncidentType Type { get { return type; } }
+    public float Timer { get { return timer; } }
+    public Module Origi { get { return origin; } }
+
+    public Incident(IncidentType type, Module origin)
+    {
+        this.type = type;
+        this.origin = origin;
+        this.timer = 0;
+    }
+
+    public void Tick()
+    {
+        timer += Time.deltaTime;
+    }
+
+}
