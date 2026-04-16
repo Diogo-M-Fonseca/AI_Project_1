39c6d9084a8e0c2b77fe951322dc1cdf72e22297
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2026-04-16 18:52:30 +0100

    incident manager updated (still not working as intended)

--- Assets/Scripts/IncidentManager.cs
+++ Assets/Scripts/IncidentManager.cs
@@ -13,4 +13,39 @@ public class IncidentManager : MonoBehaviour
         instance = this; 
     }
 
+
+    private void ApplyIncident(Module module, IncidentType type)
+    {
+        switch (type)
+        {
+            case IncidentType.Fire:
+                module.SetState(ModuleState.Blocked);
+                break;
+
+            case IncidentType.OxygenLeak:
+                module.SetState(ModuleState.Dangerous);
+                break;
+
+            case IncidentType.PowerFailure:
+                module.SetState(ModuleState.Blocked);
+                break;
+        }
+    }
+
+
+    private void TriggerIncident(IncidentType type, Module module)
+    {
+        incidents.Add(new Incident(type, module));
+
+        ApplyIncident(module, type);
+    }
+
+    private void UpdateIncidents()
+    {
+        for (int i = 0; i < incidents.Count; i++)
+        {
+            incidents[i].Tick();
+        }
+    }
+
 }
