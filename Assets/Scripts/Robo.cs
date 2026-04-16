a34e48da5974fb620ad56cd3b2215ad3b767ea5c
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2026-04-17 00:26:26 +0100

    robo script base

--- Assets/Scripts/Robo.cs
+++ Assets/Scripts/Robo.cs
@@ -2,5 +2,58 @@ using UnityEngine;
 
 public class Robo : MonoBehaviour
 {
-  
+    private NavMeshAgent agent;
+    private AgentState state;
+
+    private Module[] modules;
+    private Module targetModule;
+
+    private float battery = 100f;
+    private float timer;
+
+
+
+    private void ChangeBattery()
+    {
+        battery -= Time.deltaTime * 1.5f;
+        
+        if (battery < 20f && state != AgentState.Charging)
+        {
+             ChangeState()  
+        }
+    }
+
+    private void ChangeState(AgentState novoEstado)
+    {
+        if (state == novoEstado) return;
+
+        if (state == AgentState.Moving)
+        {
+            agent.ResetPath();
+        }
+
+        state = novoEstado;
+        timer = 0f;
+
+        OnEnterState()
+    }
+
+    private void OnEnterState(AgentState novoEstado)
+    {
+        switch (novoEstado)
+        {
+            case AgentState.Moving:
+                if (targetModule != null)
+                {
+                    agent.SetDestination(targetModule.transform.position);
+                }
+                break;
+            case AgentState.Charging:
+                if (targetModule != null)
+                {
+                    agent.SetDestination(targetModule.transform.position);
+                }
+                break;
+        }
+    }
 }
