3e6ce095999a52bef14f0ec81a2ee9202a5ecdb6
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2026-04-17 18:52:12 +0100

    added code comments and corrected small spelling mistakes

--- Assets/Scripts/Incident.cs
+++ Assets/Scripts/Incident.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class Incident 
 {
     private IncidentType type;
+
+    //origin of incident
     private Module origin;
+
+    //time since the start of the incident
     private float timer;
 
+    //public getters
     public IncidentType Type { get { return type; } }
     public float Timer { get { return timer; } }
     public Module Origin { get { return origin; } }
 
     public Incident(IncidentType type, Module origin)
     {
+        //inicializes incident information
         this.type = type;
         this.origin = origin;
         this.timer = 0;
@@ -19,6 +25,7 @@ public class Incident
 
     public void Tick()
     {
+        //increases incident duration over time
         timer += Time.deltaTime;
     }
 
